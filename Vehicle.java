27742f8de537b80679c5a23eaad2237180a67ae3
Sara Usevska <sarausevska42@yahoo.com>
2023-12-12 13:46:06 +0100

    first commit

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+package project33;
+
+public class Vehicle {
+	  private String color{}
+
+	    // Constructor with a default color Vehicle() {
+	        color = "Blue"; // Set a default color, e.g., "Blue"
+	    }
+
+	    // Constructor with a parameter to set the color Vehicle(String a) {
+	        color = a;
+	    }
+	}
