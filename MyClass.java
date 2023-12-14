1615cbcfba9e96c3fd8eeec66668c840f64b00f6
Sara Usevska <sarausevska42@yahoo.com>
2023-12-14 18:07:03 +0100

    first commit

--- MyClass.java
+++ MyClass.java
@@ -1,12 +1,12 @@
 package project33;
 
-public class MyClass {
-
-	public static void main(String[] args) {
+public class MyClass{
+	
+public static void main(String[]args) {
         Vehicle v1 = new Vehicle(); // Using default color "Blue"
         Vehicle v2 = new Vehicle("Red"); // Setting color to "Red"
         
-        System.out.println("Vehicle color: " + v1); // Prints "Vehicle color: Blue"
-        System.out.println("Vehicle color: " + v2); // Prints "Vehicle color: Red"
+        System.out.println("Vehicle color: " + "v1"); // Prints "Vehicle color: Blue"
+        System.out.println("Vehicle color: " + "v2"); // Prints "Vehicle color: Red"
     }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -1,13 +1,17 @@
 package project33;
 
 public class Vehicle {
-	  private String color{}
+	private String color;
+	 // Constructor with a default color 
+	Vehicle() {
+   color = "Blue"; // Set a default color, e.g., "Blue"
+}
 
-	    // Constructor with a default color Vehicle() {
-	        color = "Blue"; // Set a default color, e.g., "Blue"
-	    }
+//Constructor with a parameter to set the color
+	Vehicle(String a) {
+   color = a;
+}
+}
+	
 
-	    // Constructor with a parameter to set the color Vehicle(String a) {
-	        color = a;
-	    }
-	}
+
